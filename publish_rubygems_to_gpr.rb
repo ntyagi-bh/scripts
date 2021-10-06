2a91fec7397dbd13475abd1cda768e12a650397f
Nipendar Tyagi <ntyagi@appriss.com>
2021-10-05 22:44:17 -0400

    updated to update the gemspec at tagged code to point to github instead of prvs host

--- publish_rubygems_to_gpr.rb
+++ publish_rubygems_to_gpr.rb
@@ -1,3 +1,5 @@
+puts "\nrun this script from local gem repo as:"
+puts "\t ruby publish_rubygems_to_gpr <github reponame, if different than local> <main_branch, if not master>\n\n"
 begin
   require 'highline/import'
 rescue LoadError
@@ -13,14 +15,16 @@ def review(msg=nil)
   exit unless confirm.downcase == 'y'
 end
 
-main_branch = ARGV[0] || 'master'
-gemname =  `pwd`.split('/').last.tr("\n", "\t").strip.split("_gem").first
+gemname =  ARGV[0] || `pwd`.split('/').last.tr("\n", "\t").strip
+main_branch = ARGV[1] || 'master'
+
+gemspec_name = Dir.glob(File.join('*.gemspec')).first
 tags = `git tag`.split(' ')
 
 puts "\npulling latest from #{main_branch}"
-puts `git co #{main_branch}`
-puts `git pull`
-puts `git fetch --all --tags`
+`git co #{main_branch} -q`
+`git pull -q`
+`git fetch --all --tags`
 puts "\nrepo is pointing to:"
 puts `git remote -v`
 
@@ -29,19 +33,44 @@ review()
 
 tags.each do |tag|
   puts "\n\n\nBUILDING FOR TAG #{tag} ..."
-  puts `git checkout tags/#{tag}`
+  `git checkout tags/#{tag}`
+
+  puts "temporarily removing allowd_push_host, homepage_uri, source_code_uri, changelog_uri..."
+  `sed -i '' '/allowed_push_host/d' #{gemspec_name}`
+  `sed -i '' '/homepage/d' #{gemspec_name}`
+  `sed -i '' '/homepage_uri/d' #{gemspec_name}`
+  `sed -i '' '/source_code_uri/d' #{gemspec_name}`
+  `sed -i '' '/changelog_uri/d' #{gemspec_name}`
+
+  puts "temporarily changing github_repo..."
+  `sed -i '' '/github_repo/d' #{gemspec_name}`
+  baseline = `grep -n 'Gem::Specification.new' #{gemspec_name}`
+  add_to_line = baseline.split(':').first.to_i + 1
+  gemspec_var = baseline.split('|')[1]
+  `sed -i '' '6s/$/\\nspec.metadata["github_repo"]="ssh:\\/\\/github.com\\/bamboohealth\\/#{gemname}"/' #{gemspec_name}`
+
   puts "Building for tag: #{tag}"
-  puts `gem build #{gemname}.gemspec -q`
+  `gem build #{gemspec_name} -q`
+  `git co .`
 end
 `git co master`
 
 packages = `ls *.gem`.split(' ')
 puts "\nFollowing packages were built: #{packages.join(', ')}"
-puts("\n\n!!! Disclaimer: If tagged code gemspec metadata.allowed_push_host exist and is not pointing to GPR, gem will not publish !!!")
-puts "\nAlso make sure *.gem is not listed in .gitignore\n\n"
+puts "\mmake sure *.gem is not listed in .gitignore\n\n" if packages.empty?
+
 review('Ready to publish?')
 
 packages.each do |pkg|
   puts "\n\n\nPUBLISHING #{pkg} ..."
-  `gem push --key github --host https://rubygems.pkg.github.com/bamboohealth #{pkg}`
+  puts `gem push --key github --host https://rubygems.pkg.github.com/bamboohealth #{pkg}`
 end
+
+built_gemname = gemspec_name.split('.gemspec').first
+tags.each do |tag|
+  next if packages.include?("#{built_gemname}-#{tag[1..-1]}.gem")
+
+  puts "\n!!! ALERT: #{tag} was not built and published."
+  puts "\t'git checkout tags/#{tag}' and ensure that version matches the tag\n"
+end
+
